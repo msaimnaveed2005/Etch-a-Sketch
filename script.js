ec47f4b349ec63561e359279808d01451d3707f0
Muhammad Saim Naveed <msaimnaveed2005@gmail.com>
2026-04-17 01:11:11 +0500

    Add Hover Trail On each Square

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ for (let i = 0; i < 256; i++)
 {
   const square = document.createElement("div");
    square.classList.add("square");
- 
+   square.addEventListener("mouseover",function()
+{
+    square.style.backgroundColor="black";
+});
   container.appendChild(square);
 }
